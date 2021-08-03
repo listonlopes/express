a18ff63a83f1dfcf6edb306ff91bc37db023697b
Liston Lopes <listonlopes1111@gmail.com>
2021-08-03 15:47:49 +0530

    modified data

--- data/data.js
+++ data/data.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 var product = require('./product.json');
 
 function writeData(prod) {
-    fs.writeFileSync(__dirname+'/product.json', JSON.stringify(prod), 'utf8');
+    fs.writeFileSync(__dirname + '/product.json', JSON.stringify(prod), 'utf8');
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -4,33 +4,6 @@ const app = require('./app');
 const port = 3000;
 const hostname = 'localhost';
 
-// app.use(express.json({
-//     verify: (req, res, buffer, encoding) => {
-//         try {
-//             JSON.parse(buffer);
-//         } catch(error) {
-//         	console.log(error.message);
-//             res.writeHead(400, { 'Content-Type': 'application/json' });
-//             return res.end(JSON.stringify({ error: 'Not valid JSON' }));
-//         }
-//     }
-// }));
-
-// const getProducts = require('./routes/getProducts');
-// app.get('/Products', getProducts);
-
-// const createProduct = require('./routes/createProduct');
-// app.post('/Product', createProduct);
-
-// const getProduct = require('./routes/getProduct');
-// app.get('/Product/:id', getProduct);
-
-// const updateProduct = require('./routes/updateProduct');
-// app.put('/Product/:id', updateProduct);
-
-// const deleteProduct = require('./routes/deleteProduct');
-// app.delete('/Product/:id', deleteProduct);
-
 const server = app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
